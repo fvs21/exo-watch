16bb38b447215acac5b73ffb5a378e69a7a7b924
ItsAlexFS <alexfitzsolis123@gmail.com>
2025-10-04 09:21:05 -0600

    added predict and change endpoints

--- backend/app/data/service.py
+++ backend/app/data/service.py
@@ -1,8 +1,20 @@
-from . import schemas
-import pandas as pd
-from typing import List, Dict
+import os
 from pydantic import BaseModel
 
 
-CURRENT_MODEL_PATH="RUTA DE LMODELO"
+BASE_DIR=os.path.dirname(os.path.dirname(__file__))
+MODELOS=os.path.join(BASE_DIR, "..", "model")
+
+_modelo_activo = str(os.path.join(MODELOS, "exoplanet_kepler_model.joblib"))
+
+class ChangeModel(BaseModel):
+    model_path: str
+
+def get_modelo_activo() -> str:
+    return _modelo_activo
+
+def cambiar_modelo(req: ChangeModel) -> dict:
+    global _modelo_activo
+    _modelo_activo = os.path.join(MODELOS, req.model_path)
+    return {"Nuevo modelo": req.model_path}
 
