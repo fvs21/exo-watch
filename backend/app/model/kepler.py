5d28e988ee66eab0698ecda045ce70e6140d6a5f
Danieeel0601 <a23216417@alumnos.uady.mx>
2025-10-04 21:46:42 -0600

    Generating new models

--- backend/app/model/kepler.py
+++ backend/app/model/kepler.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import lightgbm as lgb
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, roc_auc_score, precision_recall_curve, auc
 import matplotlib.pyplot as plt
 import joblib
@@ -110,40 +111,80 @@ def use_light_gbm_model(X: pd.DataFrame, y: pd.Series, model_params: dict = None
 
     return model_filename, accuracy, roc_auc, pr_auc
 
-def use_xg_boost_model(X: pd.DataFrame, y: pd.Series):
+def use_xg_boost_model(X: pd.DataFrame, y: pd.Series, model_params: dict = None) -> Tuple[str, float, float, float]:
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.25, random_state=42, stratify=y
     )
 
-    model = xgb.XGBClassifier(
-        objective="binary:logistic",
-        eval_metric="auc",
-        n_estimators=2000,
-        learning_rate=0.02,
-        max_depth=8,           # deeper trees
-        subsample=0.8,
-        colsample_bytree=0.8,
-        scale_pos_weight=(y_train.value_counts()[0] / y_train.value_counts()[1]),
-        reg_lambda=1.0,        # L2 regularization
-        reg_alpha=0.1,         # L1 regularization
-        random_state=42,
-        n_jobs=-1
-    )
+    model = xgb.XGBClassifier(**model_params)
 
     model.fit(X_train, y_train, eval_set=[(X_test, y_test)])
 
     y_pred = model.predict(X_test)
     y_proba = model.predict_proba(X_test)[:,1]
 
+    roc_auc = roc_auc_score(y_test, y_proba)
+    precision, recall, _ = precision_recall_curve(y_test, y_proba)
+    pr_auc = auc(recall, precision)
+
     print("ROC-AUC:", roc_auc_score(y_test, y_proba))
     print("\nReporte de clasificación:\n", classification_report(y_test, y_pred))
 
     # 5. Visualizar la importancia de las características
-    xgb.plot_importance(model)
-    plt.title("XGBoost Feature Importance")
-    plt.tight_layout()
-    plt.savefig('feature_importance_kepler_xgb.png')
-    print("Gráfico de importancia de características guardado como 'feature_importance_kepler_xgb.png'")
+    outputs = os.listdir(OUTPUTS_PATH)
+
+    if len(outputs) == 0:
+        model_filename = 'exoplanet_kepler_model_xgb.joblib'
+    else:
+        model_filename = f"exoplanet_kepler_model_v{len(outputs)+1}.joblib"
+
+    model_path = os.path.join(OUTPUTS_PATH, model_filename)
+    joblib.dump(model, model_path)
+    print(f"\nModelo guardado exitosamente como '{model_filename}'")
+
+    return model_filename, accuray, roc_auc, pr_auc 
+
+def use_randomforest_model(X: pd.DataFrame, y: pd.Series, model_params: dict = None) -> Tuple[str, float, float, float]:
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=42, stratify=y
+    )
+    # n_jobs=-1 usa todos los núcleos de tu CPU para acelerar el entrenamiento
+    model = RandomForestClassifier(**model_params )
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+
+    print(f"Precisión de Random Forest: {accuracy * 100:.2f}%")
+
+    print(classification_report(y_test, y_pred, target_names=['FALSE POSITIVE', 'CANDIDATE'])) 
+    y_pred_proba = model.predict_proba(X_test)
+
+    y_proba = y_pred_proba[:, 1]
+
+    roc_auc = roc_auc_score(y_test, y_proba)
+    precision, recall, _ = precision_recall_curve(y_test, y_proba)
+    pr_auc = auc(recall, precision)
+
+    print(f"ROC-AUC: {roc_auc:.3f}")
+    print(f"PR-AUC: {pr_auc:.3f}") 
+
+    outputs = os.listdir(OUTPUTS_PATH)
+
+    if len(outputs) == 0:
+        model_filename = 'exoplanet_kepler_model_randomforest.joblib'
+    else:
+        model_filename = f"exoplanet_kepler_model_v{len(outputs)+1}.joblib"
+
+    model_path = os.path.join(OUTPUTS_PATH, model_filename)
+
+    joblib.dump(model_rf, model_path)
+
+    print(f"\nModelo guardado exitosamente como '{model_filename}'") 
+
+    return model_filename, accuracy, roc_auc, pr_auc
 
 def train_and_evaluate_model(model_type: str = "light_gbm", params: dict = None):
     X, y = load_kepler_data()
@@ -152,8 +193,10 @@ def train_and_evaluate_model(model_type: str = "light_gbm", params: dict = None)
         return use_light_gbm_model(X, y, model_params=params)
     elif model_type == "xgboost":
         return use_xg_boost_model(X, y, model_params=params)
+    elif model_type == "random_forest":
+        return use_randomforest_model(X, y, model_params = params)
     else:
-        raise ValueError("Modelo no soportado. Usa 'light_gbm' o 'xgboost'.")
+        raise ValueError("Modelo no soportado. Usa 'light_gbm', 'xgboost' o 'random_forest'.")
 
 
 def main():
@@ -163,15 +206,40 @@ def main():
     else:
         model_to_use = sys.argv[1]
 
-    params = dict(
-        random_state=42,
-        learning_rate=0.05,
-        n_estimators=2000,
-        num_leaves=40,
-        feature_fraction=0.8,
-        lambda_l1=0.1,
-        lambda_l2=0.1,
-    ) if model_to_use == "light_gbm" else None
+    params = None
+    if model_to_use == "light_gbm":
+        params = dict(
+            random_state=42,
+            learning_rate=0.05,
+            n_estimators=1000, # Reducido para un entrenamiento más rápido
+            num_leaves=40,
+            feature_fraction=0.8,
+            lambda_l1=0.1,
+            lambda_l2=0.1,
+            n_jobs=-1
+        )
+    elif model_to_use == "xgboost":
+        params = dict(
+            objective="binary:logistic",
+            eval_metric="auc",
+            n_estimators=1000, # Reducido para un entrenamiento más rápido
+            learning_rate=0.02,
+            max_depth=8,
+            subsample=0.8,
+            colsample_bytree=0.8,
+            reg_lambda=1.0,
+            reg_alpha=0.1,
+            random_state=42,
+            n_jobs=-1
+        )
+    elif model_to_use == "random_forest":
+        params = dict(
+            n_estimators=300, # Un buen punto de partida
+            max_depth=15,
+            min_samples_leaf=5,
+            random_state=42,
+            n_jobs=-1
+        )
 
     train_and_evaluate_model(model_type=model_to_use, params=params)
 
